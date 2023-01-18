3e74159b270b7fbd63492690972fe3a1bbfa30f4
trisout78 <81978700+trisout78@users.noreply.github.com>
2023-01-18 20:34:50 +0100

    Create pufferpanel.sh

---
+++ pufferpanel.sh
@@ -0,0 +1,14 @@
+echo "This script is made by Trisout for vpsfree.fr"
+echo "Install dependencies..."
+curl -s https://packagecloud.io/install/repositories/pufferpanel/pufferpanel/script.deb.sh | sudo bash
+apt update
+echo "Install PufferPanel"
+sudo apt-get install pufferpanel
+sudo systemctl enable pufferpanel
+echo "User Setup"
+sudo pufferpanel user add
+sudo systemctl enable --now pufferpanel
+echo "---------------------------"
+echo "You can now use PufferPanel"
+echo "http://[yourserverip]:8080"
+echo "---------------------------"
